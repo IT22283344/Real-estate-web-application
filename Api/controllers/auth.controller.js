5ea70cd3a78198a41b3a60829385ddb3818cab8d
unknown <it22283344@my.sliit.lk>
2024-01-31 09:20:24 +0530

    test12

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,8 +1,8 @@
-import User from '../models/model.js'
+import User from '../models/user.model.js'
 
 export const signup=async(res,req)=>{
-    const{name,email,password}=req.body;
+    const { username,email,password}=req.body;
     const newUser=new User({username,email,password});
     await newUser.save();
-    res.status(201).json({message:"User created successfully!"});
+    res.status(201).json({message:"User created successfully"});
 };
--- Api/index.js
+++ Api/index.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("Connected to mongo is successfully!!");
+    console.log("Connected to mongo is successfully");
 }).catch((err)=>{
     console.log(err);
 });
