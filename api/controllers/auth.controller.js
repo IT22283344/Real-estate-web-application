f6dfb9c04b79461f74ad55071e65e69d1cefa879
unknown <it22283344@my.sliit.lk>
2024-02-17 12:27:39 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
+import  Jwt  from "jsonwebtoken";
 
 export const signup=async (req,res,next)=>{
     const {username,email,password} =req.body;
@@ -15,3 +16,21 @@ try{
 }
     
 } ;
+
+export const signin=async(req,res,next)=>{
+    const{email,password}=req.body;
+    try{
+        const validUser=await User.findOne({email});
+        if(!validUser) return next(errorHandler(401,'User not found'));
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Invalid password'));
+        const token=Jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const{password:hashedpassword,...rest}=validUser._doc;
+        const Expirydate=new Date(Date.now(),3600000);//1 hour 
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+        
+
+    }catch(error){
+        next(error);
+    }
+}
